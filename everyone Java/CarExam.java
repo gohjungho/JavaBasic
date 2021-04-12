6f39eac1bf6ffcb914666f64f9523257b1a33309
gohjungho <jerry940808@gmail.com>
2021-04-12 15:45:50 +0900

    04.12

--- everyone Java/CarExam.java
+++ everyone Java/CarExam.java
@@ -13,5 +13,23 @@ public class CarExam {
         System.out.println(System.identityHashCode(str2));
         System.out.println(System.identityHashCode(str3));
         System.out.println(System.identityHashCode(str4));
+
+        if (str1 == str2) {
+            System.out.println("주소가 같습니다.");
+        } else {
+            System.out.println("주소가 다릅니다.");
+        }
+
+        if (str3 == str4) {
+            System.out.println("주소가 같습니다.");
+        } else {
+            System.out.println("주소가 다릅니다.");
+        }
+
+        if (str3.equals(str4)) {
+            System.out.println("주소가 같습니다.");
+        } else {
+            System.out.println("주소가 다릅니다.");
+        }
     }
 }
