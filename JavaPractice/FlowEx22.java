01cf7a26c87fbcbdbc3345410f4435ceff5f1b12
gohjungho <jerry940808@gmail.com>
2021-04-20 22:39:00 +0900

    오류 수정

--- JavaPractice/FlowEx22.java
+++ JavaPractice/FlowEx22.java
@@ -1,14 +1,15 @@
 class FlowEx22 {
-    public static void main(Strign[] args) {
-        int[] arr = 0;
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
         int sum = 0;
 
-        for(int i = 0; i < arr.length; i++);
-            system.out.printf("%d ", arr[i]);
+        for (int i = 0; i < arr.length; i++) 
+            System.out.printf("%d ", arr[i]);
+            
         System.out.println();
-
-        for(int tmp : arr) {
-            System.out.printf("%d", tmp);
+        
+        for (int tmp : arr) {
+            System.out.printf("%d ", tmp);
             sum += tmp;
         }
         System.out.println();
